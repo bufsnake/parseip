fc19e9d32da7bfecf92aef2bd888e375453449cb
bufsnake <zhangquan@qq.com>
2021-04-16 18:54:16 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package parseip
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"strconv"
 	"strings"
 )
@@ -14,52 +16,59 @@ import (
 // 191.168.113.159-192.168.114.254
 // 192.167.113.159-192.168.114.254
 // 192.168.113.159-192.168.114.254
-
-func ParseIP(ip string) []string {
+func ParseIP(ip string) (startx uint32, endx uint32, err error) {
 	if strings.Contains(ip, "-") {
 		if len(strings.Split(ip, "-")[1]) <= 3 {
-			return MultipleIP(ip)
+			return multipleip(ip)
 		} else {
-			return MultipleIP2(ip)
+			return multipleip2(ip)
 		}
 	} else if strings.Contains(ip, "/") {
-		return MultipleIP3(ip)
+		return multipleip3(ip)
 	} else {
-		return SingleIP(ip)
+		return singleip(ip)
 	}
 }
 
 // 192.168.113.159
-func SingleIP(ip string) []string {
-	var ret = make([]string, 0)
+func singleip(ip string) (startx uint32, endx uint32, err error) {
 	for _, val := range strings.Split(ip, ".") {
 		ips, err := strconv.Atoi(val)
 		if err != nil {
-			panic(err.Error() + " ip parse error")
+			return 0, 0, errors.New(ip + " " + err.Error() + " ip parse error")
 		}
 		if ips > 255 {
-			panic(ip + " ip parse error")
+			return 0, 0, errors.New(ip + " ip parse error")
 		}
 	}
-	ret = append(ret, ip)
-	return ret
+	startx, err = ip2UInt32(ip)
+	if err != nil {
+		return 0, 0, err
+	}
+	endx, err = ip2UInt32(ip)
+	if err != nil {
+		return 0, 0, err
+	}
+	return startx, endx, nil
 }
 
 // 192.168.113.159-255
-func MultipleIP(ips string) []string {
-	var ret = make([]string, 0)
+func multipleip(ips string) (startx uint32, endx uint32, err error) {
 	host := strings.Split(ips, "-")
 	ip := host[0]
+	if len(strings.Split(ip, ".")) != 4 {
+		return 0, 0, errors.New("multipleip error " + ips)
+	}
 	start, err := strconv.Atoi(strings.Split(ip, ".")[3])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, 0, errors.New(ips + " " + err.Error() + " ip parse error")
 	}
 	end, err := strconv.Atoi(host[1])
 	if err != nil {
-		panic("ip parse error")
+		return 0, 0, errors.New(ips + " " + err.Error() + " ip parse error")
 	}
 	if start > end {
-		panic(ips + " ip parse error")
+		return 0, 0, errors.New(ips + " ip parse error")
 	}
 	if start < 0 {
 		start = 0
@@ -67,69 +76,50 @@ func MultipleIP(ips string) []string {
 	if end > 255 {
 		end = 255
 	}
-	for i := start; i <= end; i++ {
-		temp := strings.Split(ip, ".")[:3]
-		ret = append(ret, temp[0]+"."+temp[1]+"."+temp[2]+"."+strconv.Itoa(i))
+	temp := strings.Split(ip, ".")
+	start_t, err := ip2UInt32(temp[0] + "." + temp[1] + "." + temp[2] + "." + strconv.Itoa(start))
+	if err != nil {
+		return 0, 0, err
 	}
-	return ret
+	end_t, err := ip2UInt32(temp[0] + "." + temp[1] + "." + temp[2] + "." + strconv.Itoa(end))
+	if err != nil {
+		return 0, 0, err
+	}
+	return start_t, end_t, nil
 }
 
 // 192.168.113.159-192.168.113.254
-func MultipleIP2(ips string) []string {
-	var ret = make([]string, 0)
-	start := strings.Split(strings.Split(ips, "-")[0], ".")
-	end := strings.Split(strings.Split(ips, "-")[1], ".")
-	for i := 0; i < 3; i++ {
-		if start[i] != end[i] {
-			return MultipleIP4(ips)
-		}
-	}
-	temp1, err := strconv.Atoi(start[3])
+func multipleip2(ips string) (startx uint32, endx uint32, err error) {
+	start, err := ip2UInt32(strings.Split(ips, "-")[0])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, 0, err
 	}
-	temp2, err := strconv.Atoi(end[3])
+	end, err := ip2UInt32(strings.Split(ips, "-")[1])
 	if err != nil {
-		panic(err.Error() + "ip parse error")
+		return 0, 0, err
 	}
-	if temp1 > temp2 {
-		panic(ips + " ip parse error")
+	if start > end {
+		return 0, 0, errors.New(ips + " error")
 	}
-	if temp1 < 0 {
-		temp1 = 0
-	}
-	if temp1 >= 255 {
-		temp1 = 254
-	}
-	if temp2 < 0 {
-		temp2 = 0
-	}
-	if temp2 >= 255 {
-		temp2 = 254
-	}
-	for i := temp1; i <= temp2; i++ {
-		ret = append(ret, start[0]+"."+start[1]+"."+start[2]+"."+strconv.Itoa(i))
-	}
-	return ret
+	return start, end, nil
 }
 
 // 192.168.113.0/24
-func MultipleIP3(ips string) []string {
-	var ret = make([]string, 0)
+func multipleip3(ips string) (startx uint32, endx uint32, err error) {
 	host := strings.Split(ips, "/")[0]
 	mask, err := strconv.Atoi(strings.Split(ips, "/")[1])
 	if err != nil {
-		panic(ips + " ip parse error")
+		return 0, 0, errors.New(ips + " " + err.Error() + " ip parse error")
 	}
 	if len(strings.Split(host, ".")) != 4 {
-		panic(ips + " ip parse error")
+		return 0, 0, errors.New(ips + " ip parse error")
 	}
 	a, err := strconv.Atoi(strings.Split(host, ".")[0])
 	b, err := strconv.Atoi(strings.Split(host, ".")[1])
 	c, err := strconv.Atoi(strings.Split(host, ".")[2])
 	d, err := strconv.Atoi(strings.Split(host, ".")[3])
 	if err != nil {
-		panic(ips + " ip parse error")
+		return 0, 0, errors.New(ips + " ip parse error")
 	}
 	ipbin := fmt.Sprintf("%08s", strconv.FormatInt(int64(a), 2)) +
 		fmt.Sprintf("%08s", strconv.FormatInt(int64(b), 2)) +
@@ -143,109 +133,53 @@ func MultipleIP3(ips string) []string {
 		end += "1"
 	}
 	start1, err := strconv.ParseUint(start, 2, 32)
-	end2, err := strconv.ParseUint(end, 2, 32)
-	for i := start1; i <= end2; i++ {
-		temp := fmt.Sprintf("%08s", strconv.FormatInt(int64(i), 16))
-		e, err := strconv.ParseUint(temp[0:0+2], 16, 8)
-		f, err := strconv.ParseUint(temp[2:2+2], 16, 8)
-		g, err := strconv.ParseUint(temp[4:4+2], 16, 8)
-		h, err := strconv.ParseUint(temp[6:6+2], 16, 8)
-		if err != nil {
-			panic(err.Error() + " ip parse error")
-		}
-		ret = append(ret, strconv.Itoa(int(e))+"."+strconv.Itoa(int(f))+"."+strconv.Itoa(int(g))+"."+strconv.Itoa(int(h)))
+	if err != nil {
+		return 0, 0, errors.New(ips + " ip parse error: " + err.Error())
 	}
-	return ret
+	end2, err := strconv.ParseUint(end, 2, 32)
+	if err != nil {
+		return 0, 0, errors.New(ips + " ip parse error: " + err.Error())
+	}
+	return uint32(start1), uint32(end2), nil
 }
 
-// 191.168.113.159-192.168.114.254
-// 192.167.113.159-192.168.114.254
-// 192.168.113.159-192.168.114.254
-func MultipleIP4(ips string) []string {
-	var ret = make([]string, 0)
-	start := strings.Split(strings.Split(ips, "-")[0], ".")
-	end := strings.Split(strings.Split(ips, "-")[1], ".")
-	var i = 0
-	for i = 0; i < 3; i++ {
-		if start[i] != end[i] {
-			break
-		}
+func ip2UInt32(ipnr string) (uint32, error) {
+	bits := strings.Split(ipnr, ".")
+	if len(bits) != 4 {
+		return 0, errors.New("ip2Uint32 error " + ipnr)
 	}
-	temp1, err := strconv.Atoi(start[i])
+
+	b0, err := strconv.Atoi(bits[0])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, err
 	}
-	temp2, err := strconv.Atoi(end[i])
+	b1, err := strconv.Atoi(bits[1])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, err
 	}
-	if temp1 > temp2 {
-		panic(ips + " ip parse error")
-	}
-	a, err := strconv.Atoi(start[0])
-	b, err := strconv.Atoi(start[1])
-	c, err := strconv.Atoi(start[2])
-	d, err := strconv.Atoi(start[3])
+	b2, err := strconv.Atoi(bits[2])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, err
 	}
-	if a > 255 {
-		a = 255
-	}
-	if b > 255 {
-		b = 255
-	}
-	if c > 255 {
-		c = 255
-	}
-	if d > 255 {
-		d = 255
-	}
-	one := fmt.Sprintf("%02s", strconv.FormatInt(int64(a), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(b), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(c), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(d), 16))
-	first, err := strconv.ParseUint(one, 16, 32)
+	b3, err := strconv.Atoi(bits[3])
 	if err != nil {
-		panic(err.Error() + " ip parse error")
+		return 0, err
 	}
-	a, err = strconv.Atoi(end[0])
-	b, err = strconv.Atoi(end[1])
-	c, err = strconv.Atoi(end[2])
-	d, err = strconv.Atoi(end[3])
-	if err != nil {
-		panic(err.Error() + " ip parse error")
-	}
-	if a > 255 {
-		a = 255
-	}
-	if b > 255 {
-		b = 255
-	}
-	if c > 255 {
-		c = 255
-	}
-	if d > 255 {
-		d = 255
-	}
-	one = fmt.Sprintf("%02s", strconv.FormatInt(int64(a), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(b), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(c), 16)) +
-		fmt.Sprintf("%02s", strconv.FormatInt(int64(d), 16))
-	second, err := strconv.ParseUint(one, 16, 32)
-	if err != nil {
-		panic(err.Error() + " ip parse error")
-	}
-	for i := first; i <= second; i++ {
-		temp := fmt.Sprintf("%08s", strconv.FormatInt(int64(i), 16))
-		e, err := strconv.ParseUint(temp[0:0+2], 16, 8)
-		f, err := strconv.ParseUint(temp[2:2+2], 16, 8)
-		g, err := strconv.ParseUint(temp[4:4+2], 16, 8)
-		h, err := strconv.ParseUint(temp[6:6+2], 16, 8)
-		if err != nil {
-			panic(err.Error() + " ip parse error")
-		}
-		ret = append(ret, strconv.Itoa(int(e))+"."+strconv.Itoa(int(f))+"."+strconv.Itoa(int(g))+"."+strconv.Itoa(int(h)))
-	}
-	return ret
+
+	var sum uint32
+	sum += uint32(b0) << 24
+	sum += uint32(b1) << 16
+	sum += uint32(b2) << 8
+	sum += uint32(b3)
+	return sum, nil
+}
+
+func UInt32ToIP(intIP uint32) string {
+	var bytes [4]byte
+	bytes[0] = byte(intIP & 0xFF)
+	bytes[1] = byte((intIP >> 8) & 0xFF)
+	bytes[2] = byte((intIP >> 16) & 0xFF)
+	bytes[3] = byte((intIP >> 24) & 0xFF)
+
+	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0]).String()
 }
